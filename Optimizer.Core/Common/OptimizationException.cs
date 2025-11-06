567e0113fb06f9afa5f63d38f36144ee3ee1ec43
LUckyC10ver <shiori.yuki618@gmail.com>
2025-11-06 15:56:47 +0800

    Add XML documentation to OptimizationException class
    Added XML documentation for the OptimizationException class.

--- Optimizer.Core/Common/OptimizationException.cs
+++ Optimizer.Core/Common/OptimizationException.cs
@@ -2,20 +2,20 @@ using System;
 
 namespace Optimizer.Core.Common
 {
-    [Serializable]
+    /// <summary>
+    /// Base exception type for all optimisation errors raised by the library.
+    /// </summary>
     public class OptimizationException : Exception
     {
         public OptimizationException()
         {
         }
 
-        public OptimizationException(string message)
-            : base(message)
+        public OptimizationException(string message) : base(message)
         {
         }
 
-        public OptimizationException(string message, Exception innerException)
-            : base(message, innerException)
+        public OptimizationException(string message, Exception innerException) : base(message, innerException)
         {
         }
     }
